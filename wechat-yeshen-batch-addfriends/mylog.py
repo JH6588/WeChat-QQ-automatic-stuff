3d04dd7cc597972506775ddc018d3d2c8621de04
Hui <summerlove66@users.noreply.github.com>
2017-12-15 15:38:58 +0800

    Add files via upload

---
+++ wechat-yeshen-batch-addfriends/mylog.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def get_logger(filename ,logname=""):
+    logger = logging.getLogger(logname)
+    logger.setLevel(logging.INFO)
+
+    # create the logging file handler
+    fh = logging.FileHandler(filename=filename)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
+
+
+if __name__ == '__main__':
+    a = get_logger(filename="added.log")
+    a.info("1212121   -> 1")
